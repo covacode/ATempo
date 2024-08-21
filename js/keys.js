a01a674221c5e91dcacfb16caec7c586a2c98fbe
Sergio Cova <covascode@gmail.com>
2024-08-21 00:08:17 -0300

    Update

---
+++ js/keys.js
@@ -0,0 +1,10 @@
+// Configuración de Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCWWsVzpfPYQmjNa5kW_5_7FmwM9wjQX3Q",
+    authDomain: "atempo-sacb.firebaseapp.com",
+    projectId: "atempo-sacb",
+    storageBucket: "atempo-sacb.appspot.com",
+    messagingSenderId: "954859089085",
+    appId: "1:954859089085:web:f79f743bbd836eed7d1bd4",
+    databaseURL: "https://atempo-sacb-default-rtdb.firebaseio.com/"
+};
